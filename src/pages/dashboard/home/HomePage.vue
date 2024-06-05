8682d71da9a662c38d0b3e3a1ee2d7b4cbd214a9
Trung <trungti09121998@gmail.com>
2024-06-06 00:38:11 +0700

    add driver intro steps

--- src/pages/dashboard/home/HomePage.vue
+++ src/pages/dashboard/home/HomePage.vue
@@ -1,13 +1,13 @@
 
 <template>
     <v-container>
-        <div class="text-h4 mb-2">Home page</div>
+        <div id="tour-homepage-1" class="text-h4 mb-2">Home page</div>
         <v-row>
             <v-col cols="2">
-                <v-card>
+                <v-card id="tour-homepage-2">
                     <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px"></v-img>
-                    <v-card-title> Top western road trips </v-card-title>
-                    <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
+                    <v-card-title id="tour-homepage-3"> Top western road trips </v-card-title>
+                    <v-card-subtitle id="tour-homepage-4"> 1,000 miles of wonder </v-card-subtitle>
                 </v-card>
             </v-col>
         </v-row>
@@ -16,7 +16,9 @@
 
 <script>
 import userService from '@/apis/userService/index';
+import useDriver from '@/modules/Driver/driver';
 import { defineComponent } from 'vue';
+import { STEPS_TOUR_HOMEPAGE } from '@/modules/Driver/steps/stepsHomepage';
 
 export default defineComponent({
     components: {},
@@ -28,7 +30,9 @@ export default defineComponent({
     watch: {},
     computed: {},
     methods: {},
-    mounted() {},
+    mounted() {
+        useDriver(STEPS_TOUR_HOMEPAGE);
+    },
     created() {
         userService.getUsers().then((res) => {
             console.log(res);
