fabb8452ecf0e7b804279c6b331fff64584d6e6e
Trung <trungti09121998@gmail.com>
2024-06-06 14:09:23 +0700

    change lowercase folder

---
+++ src/modules/header/AppHeader.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-app-bar>
+        <v-toolbar-title class="text_custom">AppHeader</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon @click="handleLogout">
+            <v-icon>mdi-logout</v-icon>
+        </v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+import { mapMutations } from 'vuex';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'AppHeader',
+    props: {},
+    data() {
+        return {};
+    },
+    watch: {},
+    computed: {},
+    methods: {
+        ...mapMutations(['mutateToken']),
+        handleLogout() {
+            this.mutateToken(null);
+            if (localStorage.getItem(import.meta.env.VITE_APP_STORAGE)) {
+                localStorage.removeItem(import.meta.env.VITE_APP_STORAGE);
+            }
+            this.$router.push({ name: 'SigninPage' });
+        },
+    },
+    mounted() {},
+    created() {},
+    beforeDestroy() {},
+});
+</script>
+
+<style lang="scss" scoped>
+</style>
+
