f6c15c75a7ffbb1b2a54a2f11d8d482c4a14a94a
Trung <trungti09121998@gmail.com>
2024-06-06 18:38:10 +0700

    fix

---
+++ src/modules/toast/toastContants.js
@@ -0,0 +1,13 @@
+export const ToastTypes = {
+    SUCCESS: 'success',
+    ERROR: 'error',
+    WARNING: 'warning',
+    INFO: 'info',
+};
+
+export const ToastIcons = {
+    SUCCESS: 'mdi-check-circle',
+    ERROR: 'error',
+    WARNING: 'warning',
+    INFO: 'info',
+};
