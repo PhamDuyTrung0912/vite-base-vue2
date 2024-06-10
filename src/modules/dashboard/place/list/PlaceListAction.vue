14b42bb66246e5d98aeb55373279c8314c342ae3
nguyenconganhel97 <nguyenconganhel97@gmail.com>
2024-06-10 17:51:11 +0700

    process FE place

--- src/modules/dashboard/place/list/PlaceListAction.vue
+++ src/modules/dashboard/place/list/PlaceListAction.vue
@@ -1,5 +1,26 @@
 <template>
-    <div>PlaceListAction</div>
+    <div>
+        <div>
+            <v-container>
+                <v-row no-gutters class="d-flex justify-start">
+                    <v-btn elevation="2" large class="pa-2 " outlined color="secondary">
+                        <v-icon class="px-1">mdi-plus-circle-outline</v-icon
+                        ><span class="text_primary_darken--text text-subtitle-1 px-2"> Créer </span>
+                    </v-btn>
+
+                    <v-btn elevation="2" large class="pa-2 mx-5" outlined color="secondary">
+                        <v-icon class="px-1">mdi-download</v-icon
+                        ><span class="text_primary_darken--text text-subtitle-1 px-2"> Importer </span>
+                    </v-btn>
+
+                    <v-btn elevation="2" large class="pa-2" outlined color="secondary">
+                        <v-icon class="px-1">mdi-upload</v-icon
+                        ><span class="text_primary_darken--text text-subtitle-1 px-2"> Exporter </span>
+                    </v-btn>
+                </v-row>
+            </v-container>
+        </div>
+    </div>
 </template>
 
 <script>
--- src/pages/dashboard/place/PlacePage.vue
+++ src/pages/dashboard/place/PlacePage.vue
@@ -1,18 +1,18 @@
 <template>
     <v-container>
-      <div class="text-h5 font-weight-bold">Lieux</div>
-      <router-view></router-view>
+        <v-container>
+            <div class="text-h5 font-weight-bold">Lieux</div>
+        </v-container>
+        <router-view></router-view>
     </v-container>
-  </template>
-  
-  <script>
-  import { defineComponent } from 'vue';
-  
-  export default defineComponent({
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
     name: 'PlacePage',
-  });
-  </script>
-  
-  <style scoped>
-  </style>
-  
+});
+</script>
+
+<style scoped></style>
