87287c9d391a83085b7cd0d30100bd972d240b06
Trung <trungti09121998@gmail.com>
2024-06-13 23:39:54 +0700

    Design page login

--- src/pages/auth/signin/SigninPage.vue
+++ src/pages/auth/signin/SigninPage.vue
@@ -1,46 +1,21 @@
 <template>
-    <v-container>
-        <v-card width="500px" class="pa-5">
-            <v-card-title>Signin</v-card-title>
-            <v-form ref="form" on @submit.prevent="signin">
-                <v-text-field v-model="email" :rules="emailRules" label="Email" dense outlined></v-text-field>
-                <v-text-field
-                    autocomplete="on"
-                    v-model="password"
-                    :rules="passwordRules"
-                    label="Password"
-                    dense
-                    outlined
-                    type="password"></v-text-field>
-                <v-btn color="primary" type="submit">Signin</v-btn>
-            </v-form>
-        </v-card>
+    <v-container style="height: 100vh" class="d-flex align-center justify-center">
+        <signin-container />
     </v-container>
 </template>
 
 <script>
-import { mapMutations } from 'vuex';
 import { defineComponent } from 'vue';
+import SigninContainer from '@/modules/auth/SigninContainer.vue';
 
 export default defineComponent({
+    components: { SigninContainer },
     name: 'SigninPage',
     props: {},
     data() {
-        return {
-            email: '',
-            password: '',
-            emailRules: [(v) => !!v || 'Email is required', (v) => /.+@.+\..+/.test(v) || 'Email must be valid'],
-            passwordRules: [(v) => !!v || 'Password is required', (v) => (v && v.length >= 6) || 'Password must be at least 6 characters'],
-        };
+        return {};
     },
-    methods: {
-        signin() {
-            if (this.$refs.form.validate()) {
-                this.$cookies.set('token', 'TOKEN-HERE-CA');
-                this.$router.push({ name: 'HomePage' });
-            }
-        },
-    },
+    methods: {},
 });
 </script>
 
