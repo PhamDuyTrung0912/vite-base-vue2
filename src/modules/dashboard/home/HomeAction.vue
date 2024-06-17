038cf3800c4307a5e39fd6813c4513b1681a4403
Trung <trungti09121998@gmail.com>
2024-06-17 11:00:45 +0700

    add mixin media query and ui logout

--- src/modules/dashboard/home/HomeAction.vue
+++ src/modules/dashboard/home/HomeAction.vue
@@ -1,27 +1,27 @@
 <template>
     <v-container>
-        <v-row>
-            <v-col cols="12" lg="2" md="12" sm="6" v-for="(action, key) in actions" :key="key">
-                <v-card
-                    @click="changeRoute(action.route)"
-                    elevation="2"
-                    class="item-action"
-                    style="cursor: pointer"
-                    :style="{ border: 'solid 1px ' + action.color }">
-                    <v-row no-gutters class="pa-2">
-                        <v-col cols="2" class="d-flex justify-center align-center">
-                            <v-icon :color="action.color">{{ action.icon }}</v-icon>
+        <div class="d-flex align-center py-3" style="overflow-x: auto">
+            <v-card
+                v-for="(action, key) in actions"
+                :key="key"
+                @click="changeRoute(action.route)"
+                elevation="2"
+                class="item-action"
+                style="cursor: pointer"
+                :style="{ border: 'solid 1px ' + action.color }">
+                <v-row no-gutters class="pa-2">
+                    <v-col cols="2" class="d-flex justify-center align-center">
+                        <v-icon :color="action.color">{{ action.icon }}</v-icon>
+                    </v-col>
+                    <v-col cols="10" class="pa-0 px-2">
+                        <v-col cols="12" class="text-subtitle-2 pa-0 text_primary--text">
+                            {{ action.title }}
                         </v-col>
-                        <v-col cols="10" class="pa-0 px-2">
-                            <v-col cols="12" class="text-subtitle-2 pa-0 text_primary--text font-weight-bold">
-                                {{ action.title }}
-                            </v-col>
-                            <v-col cols="12" class="text-h5 pa-0 text_primary--text font-weight-bold"> {{ action.text }} </v-col>
-                        </v-col>
-                    </v-row>
-                </v-card>
-            </v-col>
-        </v-row>
+                        <v-col cols="12" class="text-h6 pa-0 text_primary--text font-weight-bold"> {{ action.text }} </v-col>
+                    </v-col>
+                </v-row>
+            </v-card>
+        </div>
     </v-container>
 </template>
 
@@ -93,5 +93,11 @@ export default defineComponent({
 <style lang="scss" scoped>
 .item-action {
     @include hover-effect(_, $primary_color);
+    margin: 0px 10px;
+    min-width: 180px;
+
+    @include screenTo(xlarge) {
+        min-width: 220px;
+    }
 }
 </style>
